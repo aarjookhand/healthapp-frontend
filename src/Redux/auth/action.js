237c9fd01e6c95938140307eab64c19bb8a52d32
Upasana Sharma <118016296+iamdealer02@users.noreply.github.com>
2024-05-06 02:42:29 +0200

    Update action.js

--- src/Redux/auth/action.js
+++ src/Redux/auth/action.js
@@ -4,22 +4,20 @@ import axios from "axios";
 //login user
 export const NurseLogin = (data) => async (dispatch) => {
   try {
-    
     dispatch({ type: types.LOGIN_NURSE_REQUEST });
     const res = await axios.post(
-      "http://localhost:3000/users/authenticate",
+      "https://zany-gray-clam-gear.cyclic.app/nurses/login",
       data
     );
-    console.log(res);
     dispatch({
       type: types.LOGIN_NURSE_SUCCESS,
       payload: {
-        message: 'Successful',
-        user: data.userName,
-        token: res.data.sessionid,
+        message: res.data.message,
+        user: res.data.user,
+        token: res.data.token,
       },
     });
-    return res;
+    return res.data;
   } catch (error) {
     dispatch({
       type: types.LOGIN_NURSE_ERROR,
@@ -120,7 +118,7 @@ export const NurseRegister = (data) => async (dispatch) => {
   try {
     dispatch({ type: types.REGISTER_NURSE_REQUEST });
     const res = await axios.post(
-      "localhost:300/users",
+      "https://zany-gray-clam-gear.cyclic.app/nurses/register",
       data
     );
     // console.log(res);
